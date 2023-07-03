949441867cf94caaa873f2dd660c5a97e06f6c4e
Viktor Ilin <viktor125142@gmail.com>
2023-07-03 18:16:49 +0300

    Слияние двух веток

--- new/new.py
+++ new/new.py
@@ -1,17 +1,3 @@
-<<<<<<< ours
 print('Way')
-
-
-def function(a, d):
-    a = 4
-    b =3
-    return a*b
-
-
-print('просто')
-||||||| base
-print('Way')
-=======
 def new_function():
     return 2*2
->>>>>>> theirs
